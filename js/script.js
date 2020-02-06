21a7a879353a6ffde14162eaaf7fb1d8f3adec76
Ghezlene Doballah <ghyzounette@gmail.com>
2020-02-06 11:36:49 +0100

    prova senza input

--- js/script.js
+++ js/script.js
@@ -10,7 +10,26 @@ $(document).ready(function() {
       query : 'ritorno al futuro'
     },
     success: function (data) {
-      console.log(data);
+      console.log(data.results);
+      var arrayFilms = data.results
+      for (var i = 0; i < arrayFilms.length; i++) {
+        var film = arrayFilms[i];
+        console.log(film);
+        var source = $('#film-template').html();
+        var template = Handlebars.compile(source);
+        var context = {
+          title: film.title,
+          original_title: film.original_title,
+          original_language: film.original_language,
+          vote_average: film.vote_average
+        };
+        var html = template(context);
+        $('.covers').append(html);
+      }
+      // original_language: "en"
+      // original_title: "Back to the Future"
+      // title: "Back to the Future"
+      // vote_average: 8.2
 
   },
     error : function (request, state, errors) {
