2e49720143564eca91e12052e017dbed556c97d2
Ghezlene Doballah <ghyzounette@gmail.com>
2020-02-10 12:15:37 +0100

    print series

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var query = $('#type').val();
     console.log(query);
     getFilms(query);
+    getSeries(query);
     reset();
   });
 
@@ -11,6 +12,7 @@ $(document).ready(function() {
       var query = $('#type').val();
       if (event.which == 13) {
         getFilms(query);
+        getSeries(query);
         reset();
       }
     }
@@ -21,21 +23,40 @@ $(document).ready(function() {
 
 // Functions--------------------------
 
-function printFilms(array) {
-  for (var i = 0; i < array.length; i++) {
-    var film = array[i];
+function printData(type, data) {
+  for (var i = 0; i < data.length; i++) {
+    var data = data[i];
+    var title;
+    var originalTitle;
     var source = $('#film-template').html();
     var template = Handlebars.compile(source);
-    var context = {
-      title: film.title,
-      original_title: film.original_title,
-      original_language: film.original_language,
-      vote_average: getRanking(film.vote_average)
+    if (type == 'film') {
+      title = data.title;
+      originalTitle = data.original_title;
+      var container = $('.films');
+    } else if (type == 'series') {
+      title = data.name;
+      originalTitle = data.original_name;
+      var container = $('.series')
     }
+    var context = {
+      type: type,
+      title: title,
+      original_title: originalTitle,
+      original_language: getLanguage(data.original_language),
+      vote_average: getRanking(data.vote_average)
+    };
     var html = template(context);
-    $('.covers').append(html);
-  };
+    container.append(html);
+  }
+}
 
+function getLanguage(language) {
+  var availableLanguage = ['de', 'en', 'es', 'fr', 'pt', 'it'];
+  if (availableLanguage.includes(language)) {
+    language = '<img src="img/' + language + '.png" alt="language">';
+  }
+  return language;
 }
 
 function getRanking(vote) {
@@ -53,8 +74,6 @@ function getRanking(vote) {
 
 }
 
-
-
 function getFilms(string) {
   var url = 'https://api.themoviedb.org/3/search/movie';
   var api_key = '5d285ad7ca8d96394a45a69142e4ce2f';
@@ -70,9 +89,35 @@ function getFilms(string) {
         console.log(data.results);
         var arrayFilms = data.results;
         if (arrayFilms.length == 0) {
-        $('.covers').text('Messaggio di Boolflix "Titolo non disponibile"');
+        $('.films').text('Messaggio di Boolflix "Titolo non disponibile"');
       } else {
-        printFilms(arrayFilms);
+        printData('film', arrayFilms);
+      }
+      },
+      error : function (request, state, errors) {
+        console.log('Errore ' + errors);
+      }
+  });
+}
+
+function getSeries(string) {
+  var url = 'https://api.themoviedb.org/3/search/tv';
+  var api_key = '5d285ad7ca8d96394a45a69142e4ce2f';
+  $.ajax(
+    {
+      url : url ,
+      method : 'GET',
+      data : {
+        api_key :api_key ,
+        query : string
+      },
+      success: function (data) {
+        console.log(data.results);
+        var arraySeries = data.results;
+        if (arraySeries.length == 0) {
+        $('.series').text('Messaggio di Boolflix "Titolo non disponibile"');
+      } else {
+        printData('series', arraySeries);
       }
       },
       error : function (request, state, errors) {
